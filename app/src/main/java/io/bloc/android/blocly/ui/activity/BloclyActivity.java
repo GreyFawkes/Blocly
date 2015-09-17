0fc5890e349a331ea116497f6854f860a1778f8d
Max - GreyFawkes <mvonthaden@gmail.com>
2015-09-17 13:36:29 -0400

    checkpoint-46 complete

--- app/src/main/java/io/bloc/android/blocly/ui/activity/BloclyActivity.java
+++ app/src/main/java/io/bloc/android/blocly/ui/activity/BloclyActivity.java
@@ -1,6 +1,7 @@
 package io.bloc.android.blocly.ui.activity;
 
 import android.content.res.Configuration;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
@@ -9,7 +10,9 @@ import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 import com.nostra13.universalimageloader.cache.memory.impl.LruMemoryCache;
@@ -17,6 +20,8 @@ import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
 import com.nostra13.universalimageloader.core.assist.QueueProcessingType;
+
+import java.util.ArrayList;
 
 import io.bloc.android.blocly.R;
 import io.bloc.android.blocly.api.model.RssFeed;
@@ -32,6 +37,8 @@ public class BloclyActivity extends ActionBarActivity implements NavigationDrawe
     private ActionBarDrawerToggle mDrawerToggle;
     private DrawerLayout mDrawerLayout;
     private NavigationDrawerAdapter mNavigationDrawerAdapter;
+    private Menu mMenu;
+    private View mOverflowButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -69,7 +76,71 @@ public class BloclyActivity extends ActionBarActivity implements NavigationDrawe
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         mDrawerLayout = (DrawerLayout) findViewById(R.id.dl_activity_blocly);
 
-        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, 0, 0);
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, 0, 0) {
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                if(mOverflowButton != null) {
+                    mOverflowButton.setAlpha(1f);
+                    mOverflowButton.setEnabled(true);
+                }
+                if(mMenu == null) {
+                    return;
+                }
+                for (int i = 0; i<mMenu.size(); i++) {
+                    MenuItem item = mMenu.getItem(i);
+                    item.setEnabled(true);
+                    Drawable icon = item.getIcon();
+                    if(icon != null) {
+                        icon.setAlpha(255);
+                    }
+                }
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+
+                if(mOverflowButton != null) {
+                    mOverflowButton.setEnabled(false);
+                }
+                if(mMenu == null) {
+                    return;
+                }
+                for(int i = 0; i<mMenu.size(); i++) {
+                    mMenu.getItem(i).setEnabled(false);
+                }
+            }
+
+            @Override
+            public void onDrawerSlide(View drawerView, float slideOffset) {
+                super.onDrawerSlide(drawerView, slideOffset);
+                if(mOverflowButton == null) {
+                    ArrayList<View> foundViews = new ArrayList<View>();
+                    getWindow().getDecorView().findViewsWithText(foundViews,
+                            getString(R.string.abc_action_menu_overflow_description),
+                            View.FIND_VIEWS_WITH_CONTENT_DESCRIPTION);
+                    if(foundViews.size() > 0) {
+                        mOverflowButton = foundViews.get(0);
+                    }
+                }
+
+                if(mOverflowButton != null) {
+                    mOverflowButton.setAlpha(1f - slideOffset);
+                }
+                if(mMenu == null) {
+                    return;
+                }
+                for (int i = 0; i < mMenu.size(); i++) {
+                    MenuItem item = mMenu.getItem(i);
+                    Drawable icon = item.getIcon();
+                    if(icon != null) {
+                        icon.setAlpha((int) ((1f - slideOffset) * 255));
+                    }
+                }
+            }
+        };
+
         mDrawerLayout.setDrawerListener(mDrawerToggle);
 
         mNavigationDrawerAdapter = new NavigationDrawerAdapter();
@@ -96,7 +167,16 @@ public class BloclyActivity extends ActionBarActivity implements NavigationDrawe
         if(mDrawerToggle.onOptionsItemSelected(item)) {
             return  true;
         }
+        Toast.makeText(this, item.getTitle(), Toast.LENGTH_SHORT).show();
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.blocly, menu);
+        mMenu = menu;
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
